577548bd69661dcf102386763732442806723e58
Connor <wxj995@gmail.com>
2026-09-15 23:28:59 -0700

    add formsample

---
+++ FlaskTest/formsample/app.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+
+from sqlalchemy.orm import DeclarativeBase #实例化扩展类时，除了程序实例，需要额外传入一个继承自 DeclarativeBase 的子类作为 model_class 参数的值。
+from sqlalchemy.orm import Mapped, mapped_column
+
+from sqlalchemy import String
+from sqlalchemy import select
+
+from pathlib import Path
+import click
+
+app = Flask(__name__)
+
+#写入了一个 SQLALCHEMY_DATABASE_URI 变量来告诉 SQLAlchemy 数据库连接地址
+#sqlite:///数据库文件的绝对地址
+#非Windows，sqlite:////绝对路径/数据库文件名.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + str(Path(app.root_path) / 'data.db')
+
+#目前这个类是空的，后续你可以按照需要对这个基类进行自定义。
+class Base(DeclarativeBase):
+  pass
+
+#初始化扩展，传入程序实例 app
+db = SQLAlchemy(app, model_class=Base)  
+
+class User(db.Model):
+    __tablename__ = 'user' # 定义表名称
+    id: Mapped[int] = mapped_column(primary_key=True)  # 主键
+    name: Mapped[str] = mapped_column(String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    __tablename__ = 'movie'
+    id: Mapped[int] = mapped_column(primary_key=True)  # 主键
+    title: Mapped[str] = mapped_column(String(60))  # 电影标题
+    year: Mapped[str] = mapped_column(String(4))  # 电影年份
+
+#生成数据库，或者删除数据库
+#python -m flask init-db
+#python -m flask init-db --drop
+@app.cli.command('init-db')  # 注册为命令，传入自定义命令名
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def init_database(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+#伪造数据库数据并重新生成数据库
+#python -m flask forge
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.drop_all()
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Daniel'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+#模板优化
+#使用 app.context_processor 装饰器注册一个模板上下文处理函数
+#这个函数返回的变量（以字典键值对的形式）将会统一注入到每一个模板的上下文环境中，因此可以直接在模板中使用。
+#后面我们创建的任意一个模板，都可以在模板中直接使用 user 变量。
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = db.session.execute(select(User)).scalar()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+
+#主页视图函数
+#http://127.0.0.1:5000/
+@app.route('/')
+def index():
+    #user = db.session.execute(select(User)).scalar()  # 读取用户记录
+    movies = db.session.execute(select(Movie)).scalars().all()  # 读取所有电影记录
+    return render_template('index.html', movies=movies)
+
+#404 错误处理函数
+#http://127.0.0.1:5000/foo
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(error):  # 接受异常对象作为参数
+    #user = db.session.execute(select(User)).scalar()
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+
+
+
+
+
